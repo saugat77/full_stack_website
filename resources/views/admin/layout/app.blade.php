a6ea96b76bb83c414f6e17bd9c2468a793373b06
Saugat pandey <saugatpandey4@gmail.com>
2023-09-03 14:47:56 +0545

    added model for client and used seeder to seed

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -209,7 +209,7 @@
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link to="/admin/ppointments" active-class="active" class="nav-link">
+                            <router-link to="/admin/appointments" active-class="active" class="nav-link">
                                 <i class="nav-icon fas fa-calendar-alt"></i>
                                 <p>
                                     Appointments
