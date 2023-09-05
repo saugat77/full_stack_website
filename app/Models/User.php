834f6654cab701c6c243b6fcb6fce6c7b6f485a3
Saugat pandey <saugatpandey4@gmail.com>
2023-09-05 13:29:10 +0545

    added default user when creating user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -43,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        // Assign the default role 'user' when creating a new user
+        $defaultRole = Role::findByName('user'); // Replace 'user' with the desired default role name
+        if ($defaultRole) {
+            $this->assignRole($defaultRole);
+        }
+    }
 }
