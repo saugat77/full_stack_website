47b65a66fd0912f6ed566e3d0458c63385d99080
saugat77 <saugatpandey4@gmail.com>
2024-04-04 13:17:47 +0545

    added sitemap.xml

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,9 +28,9 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::loginView(function(){
-            return view('auth.login');
-        });
+        // Fortify::loginView(function(){
+        //     return view('auth.login');
+        // });
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
