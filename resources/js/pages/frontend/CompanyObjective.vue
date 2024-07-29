bedd38f5224f49dce0969e8dafec45caeef9a7b4
Saugat <saugatpandey4@gmail.com>
2024-07-29 16:55:53 +0545

    :feat added company objective

---
+++ resources/js/pages/frontend/CompanyObjective.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <div class="grid grid-cols-3 gap-3">
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <!-- ... -->
+        <div>09</div>
+    </div>
+</template>
---
+++ resources/js/pages/frontend/MessageFromChairman.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+    <div class="mt-1">
+        <h1 class="bg-yellow-500 text-white rounded-lg p-2 mb-1 text-center">
+            Message From Chairman
+        </h1>
+        <div class="p-2">
+            <div class="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg overflow-hidden">
+                <div class="w-full md:w-1/2 p-4">
+                    <h1 class="text-2xl font-bold mb-2">Kantipur Overseas Services Pvt.</h1>
+                    <p class="text-gray-600">
+                        Kantipur Overseas Services Pvt. Ltd was envisioned,
+                        founded and administered for sustainable growth through
+                        a relentless effort by providing capable manpower
+                        services to business organizations all over the world.
+                        We have a management system with a highly capable team
+                        of professionals and have gained consistent stability
+                        and growth of human resources despite the crisis in
+                        recent years. We are an employment/recruitment company
+                        in Kathmandu, Nepal driven towords providing manpower to
+                        every job category in Asia, the Middle East and Europe.
+                    </p>
+                </div>
+                <div class="flex-shrink-0 w-full md:w-1/2">
+                    <img
+                        class="w-full md:w-100 h-100 object-cover"
+                        src="/assets/images/lil-sir.jpg"
+                        alt="Descriptive Image"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
